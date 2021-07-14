3552d281045f43a5d5c6d447dc3ad92d646d7dcd
Leere <ilonicmask@gmail.com>
2021-07-14 15:59:53 +0300

    Idk how to name this

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $('.plans-slider').slick({
     slidesToScroll: 1,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 1200,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
@@ -16,4 +16,35 @@ $('.plans-slider').slick({
             }
         }
         ]
+});
+$('.price-slider').slick({
+    dots: false,
+    infinite: false,
+    arrows:false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 580,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+]
 });
